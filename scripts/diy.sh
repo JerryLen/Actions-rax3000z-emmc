ce32d600bf90561180b7ac0dec92b074f03ad202
JerryLen <63991211+JerryLen@users.noreply.github.com>
2026-02-11 10:25:48 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -4,6 +4,4 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # 添加自定义源
-echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
-# 添加自定义包
-git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
+echo "src-git kenzo https://github.com/JerryLen/openwrt-packages" >> ./feeds.conf.default
